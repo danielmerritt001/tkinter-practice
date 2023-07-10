98ff86c218beff68048012fab59da1509740e8c6
Daniel Merritt <daniel.mafou@gmail.com>
2023-07-10 13:45:38 -0400

    grid vs pack practice

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import *
 root = Tk()
 
 myLabel = Label(root, text="Hello World!")
-myLabel.pack()
+myLabel.grid(row=0, column=0)
+
+myLabelGrid = Label(root, text="I don't have a name")
+myLabelGrid.grid(row=1, column=0)
 
 root.mainloop()
