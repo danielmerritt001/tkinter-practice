f235d57982690bb6ce8c2235c3b99ff8af391b78
Daniel Merritt <daniel.mafou@gmail.com>
2023-07-11 10:19:32 -0400

    button testing

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
-myLabel.grid(row=0, column=0)
+# ---------- display text with grid ----------
+# myLabel = Label(root, text="Hello World!")
+# myLabel.grid(row=0, column=0)
 
-myLabelGrid = Label(root, text="I don't have a name")
-myLabelGrid.grid(row=1, column=0)
+# myLabelGrid = Label(root, text="I don't have a name")
+# myLabelGrid.grid(row=1, column=0)
+
+myButton = Button(root, text="Click Me!")
+myButton.pack()
 
 root.mainloop()
