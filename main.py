66e8d210a92ba54555c696aa4fb2a0adf4fbf738
Daniel Merritt <daniel.mafou@gmail.com>
2023-07-11 10:51:01 -0400

    text box with button practice

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ root = Tk()
 # myLabelGrid = Label(root, text="I don't have a name")
 # myLabelGrid.grid(row=1, column=0)
 
-myButton = Button(root, text="Click Me!")
+
+e = Entry(root, width=50, bg="black", fg="white")
+e.pack()
+e.insert(0, "Enter Your Name")
+
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+myButton = Button(root, text="Enter Your Name", command=myClick)
 myButton.pack()
 
 root.mainloop()
